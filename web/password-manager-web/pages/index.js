b7db621ddef5443aea71043b6f648bdc7d18ebf7
neel <neel@neel.local>
2022-12-24 02:26:15 +0530

    initial commit for app done

--- web/password-manager-web/pages/index.js
+++ web/password-manager-web/pages/index.js
@@ -68,7 +68,7 @@ export default function Index() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             
-            {true ? <Home /> : renderAuth()}
+            {verified ? <Home /> : renderAuth()}
         </>
     )
 }
