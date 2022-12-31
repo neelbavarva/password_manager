dea8cfaee05d0f04c58477ee1c9a27ca6c5de6b1
neel <neel@neel.local>
2022-12-31 21:05:25 +0530

    new algo added for encryption & decryption

--- api/passwordRoute.js
+++ api/passwordRoute.js
@@ -4,6 +4,56 @@ const app = express()
 const router = express.Router()
 const Password = require('./password')
 const Cryptr = require('cryptr');
+
+// encryption - decryption algos
+
+const support_string_characters = "abcdefghijklmnopqrstuvwxyz";
+const support_string_numbers = "0123456789";
+const support_string_cap_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const support_string_sp_characters = "!@#$%^&*()";
+const mod = 5;
+
+function encrypt(password,key){
+    let encrypted_string = ""
+    let encrypted_key = ""
+
+    for(let i=0;i<password.length;i++){
+        encrypted_string += i%2==0 ? support_string_sp_characters.charAt(i%9) : support_string_characters.charAt(i%25)
+        encrypted_string += i%2==0 ? support_string_cap_characters.charAt(i%25) :  support_string_numbers.charAt(i%9);
+        encrypted_string += String.fromCharCode(password.charAt(i).charCodeAt(0) + 4);
+    }
+
+    for(let i=0;i<key.length;i++){
+        encrypted_key += i%2==0 ? support_string_sp_characters.charAt(i%9) : support_string_characters.charAt(i%25)
+        encrypted_key += i%2==0 ? support_string_cap_characters.charAt(i%25) :  support_string_numbers.charAt(i%9);
+        encrypted_key += String.fromCharCode(key.charAt(i).charCodeAt(0) + 4);
+    }
+
+    encrypted_string += encrypted_key;
+    return encrypted_string;
+}
+
+function decrypt(password,key){
+    let decrypted_string = "";
+    let decrypted_key = "";
+    let key_length = (key.length-1)*3 + 1;
+
+    for(let i=0;i<password.length-key_length;i++){
+        if((i+1)%3==0){
+            decrypted_string += String.fromCharCode(password.charAt(i).charCodeAt(0) - 4);
+        }
+    }
+
+    for(let i=password.length-key_length;i<password.length;i++){
+        if((i+1)%3==0){
+            decrypted_key += String.fromCharCode(password.charAt(i).charCodeAt(0) - 4);
+        }
+    }
+
+    console.log(decrypted_key)
+
+    return decrypted_key==key ? decrypted_string : "wrong_key";
+}
   
 // GET
 
@@ -83,12 +133,13 @@ router.get('/:id', async(req, res)=> {
 // POST
 
 router.post('/newPassword', async(req, res) => {
-    const cryptr = new Cryptr(req.body.key);
+    const encrypted_string = encrypt(req.body.password, req.body.key)
+    console.log(encrypted_string)
     const passowrd = new Password({
         name: req.body.name,
         email: req.body.email,
         category: req.body.category,
-        password: cryptr.encrypt(req.body.password),
+        password: encrypted_string,
         archive: req.body.archive
     })
     try{
@@ -100,9 +151,9 @@ router.post('/newPassword', async(req, res) => {
 })
 
 router.post('/decryptPassword', async(req, res)=> {
-    const cryptr = new Cryptr(req.body.key);
+    const decrypted_string = decrypt(req.body.password, req.body.key)
     try{
-        res.status(201).json(cryptr.decrypt(req.body.password))
+        res.status(201).json(decrypted_string)
     } catch(err){
         res.status(500).json({message: err.message})
     }
