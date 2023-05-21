3e3b5a0ad11110bf710f9f8cfe99d517d513f584
neel <neel@neel.local>
2023-05-21 23:23:03 +0530

    add password link - not working

--- web/p4s2w0r9z/components/Manage.js
+++ web/p4s2w0r9z/components/Manage.js
@@ -14,6 +14,8 @@ export default function Manage() {
 
     const[category, setCategory] = useState("addPassword")
     const[backup, setBackup] = useState(true)
+    const[fieldError, setFieldError] = useState(false)
+    const[submitProcess, setSubmitProcess] = useState(false)
 
     //password
     const[pName, setPName] = useState(null)
@@ -27,6 +29,36 @@ export default function Manage() {
     const[eKey, setEKey] = useState(null)
     const[ePassword, setEPassword] = useState(null)
     const[decryptRender, setDecryptRender] = useState(false)
+
+    const addNewPassword = () => {
+        if (pName==null||pEmail==null||pCategory==null||pPassword==null||pKey==null||pArchive==null){
+            setFieldError(true)
+        } else {
+            setFieldError(false)
+            setSubmitProcess(true)
+            fetch("https://password-manager-xpkf.onrender.com/passwords/newPassword",{
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 
+                    "name": pName.trim(),
+                    "email": pEmail.trim(),
+                    "password": pPassword.trim(),
+                    "key": pPassword.trim(),
+                    "category": pCategory.trim(),
+                    "archive": pArchive
+                })
+
+            })
+            .then(res=>res.json())
+            .then(result=>{
+                renderCancel()
+                setSubmitProcess(false)
+            })
+            .catch((e) => {
+                console.log("Error in POST password " + e);
+            })
+        }
+    }
 
     const fetchPasswords = () => {
         fetch(`${API}/passwords/getAllPasswords`)
@@ -88,6 +120,10 @@ export default function Manage() {
 
     useEffect(()=>{
         fetchPasswords()
+        fetch(`${API}/passwords/getAllPasswords`)
+        .then(res=>res.json())
+        .then(result=>{})
+        .catch((e) => {})
     },[])
 
     function selectCategory(){
@@ -164,7 +200,7 @@ export default function Manage() {
                     <div onClick={() => pArchive=="No" ? setPArchive("Yes") : setPArchive("No") } className={styles.touchable_field}>{pArchive}</div>
                 </div>
                 <button onClick={() => renderCancel()} className={styles.cancel_btn}>Cancel</button>
-                <button className={styles.submit_btn}>Submit</button>
+                <button onClick={() => addNewPassword()} className={`${styles.submit_btn} ${fieldError?styles.btn_warning:null}`}>{submitProcess?"Processing...":"Submit"}</button>
             </div>
         )
     }
